7744ec43a49eea32ee7b03d436b82859f9dc0a23
Jason Bock <jason@jasonbock.net>
2019-10-03 20:41:23 -0500

    Added turn left and right

--- IronBefunge.Tests/InstructionHandlers/TurnInstructionHandlerTests.cs
+++ IronBefunge.Tests/InstructionHandlers/TurnInstructionHandlerTests.cs
@@ -11,9 +11,34 @@ namespace IronBefunge.Tests.InstructionHandlers
 	{
 		internal override ImmutableArray<char> GetExpectedHandledInstructions() =>
 			ImmutableArray.Create(TurnInstructionHandler.LeftRightInstruction,
+				TurnInstructionHandler.TurnLeftInstruction, TurnInstructionHandler.TurnRightInstruction,
 				TurnInstructionHandler.UpDownInstruction);
 
 		internal override Type GetHandlerType() => typeof(TurnInstructionHandler);
+
+		[TestCase(TurnInstructionHandler.TurnRightInstruction, Direction.Right, Direction.Down)]
+		[TestCase(TurnInstructionHandler.TurnRightInstruction, Direction.Down, Direction.Left)]
+		[TestCase(TurnInstructionHandler.TurnRightInstruction, Direction.Left, Direction.Up)]
+		[TestCase(TurnInstructionHandler.TurnRightInstruction, Direction.Up, Direction.Right)]
+		[TestCase(TurnInstructionHandler.TurnLeftInstruction, Direction.Right, Direction.Up)]
+		[TestCase(TurnInstructionHandler.TurnLeftInstruction, Direction.Up, Direction.Left)]
+		[TestCase(TurnInstructionHandler.TurnLeftInstruction, Direction.Left, Direction.Down)]
+		[TestCase(TurnInstructionHandler.TurnLeftInstruction, Direction.Down, Direction.Right)]
+		public static void HandleTurnRightAndLeft(char instruction, Direction currentDiection, Direction expectedDirection)
+		{
+			var cells = new List<Cell>() { new Cell(new Point(0, 0), instruction) };
+			var stackCount = 0;
+
+			InstructionHandlerTests.Run(new TurnInstructionHandler(), cells, (context) =>
+			{
+				context.Direction = currentDiection;
+				stackCount = context.Values.Count;
+			}, (context, result) =>
+			{
+				Assert.That(context.Values.Count, Is.EqualTo(stackCount), nameof(context.Values.Count));
+				Assert.That(context.Direction, Is.EqualTo(expectedDirection), nameof(context.Direction));
+			});
+		}
 
 		[Test]
 		public static void HandleGoingDown()
--- IronBefunge/InstructionHandlers/TurnInstructionHandler.cs
+++ IronBefunge/InstructionHandlers/TurnInstructionHandler.cs
@@ -6,22 +6,35 @@ namespace IronBefunge.InstructionHandlers
 		: InstructionHandler
 	{
 		internal const char LeftRightInstruction = '_';
+		internal const char TurnLeftInstruction = '[';
+		internal const char TurnRightInstruction = ']';
 		internal const char UpDownInstruction = '|';
 
 		internal override ImmutableArray<char> GetInstructions() =>
 			ImmutableArray.Create(TurnInstructionHandler.LeftRightInstruction,
+				TurnInstructionHandler.TurnLeftInstruction, TurnInstructionHandler.TurnRightInstruction,
 				TurnInstructionHandler.UpDownInstruction);
 
 		internal override void OnHandle(ExecutionContext context)
 		{
-			context.EnsureStack(1);
-
 			switch (context.Current.Value)
 			{
 				case TurnInstructionHandler.LeftRightInstruction:
+					context.EnsureStack(1);
 					context.Direction = context.Values.Pop() == 0 ? Direction.Right : Direction.Left;
 					break;
+				case TurnInstructionHandler.TurnRightInstruction:
+					context.Direction = context.Direction == Direction.Right ? Direction.Down :
+						context.Direction == Direction.Down ? Direction.Left :
+						context.Direction == Direction.Left ? Direction.Up : Direction.Right;
+					break;
+				case TurnInstructionHandler.TurnLeftInstruction:
+					context.Direction = context.Direction == Direction.Right ? Direction.Up :
+						context.Direction == Direction.Up ? Direction.Left :
+						context.Direction == Direction.Left ? Direction.Down : Direction.Right;
+					break;
 				case TurnInstructionHandler.UpDownInstruction:
+					context.EnsureStack(1);
 					context.Direction = context.Values.Pop() == 0 ? Direction.Down : Direction.Up;
 					break;
 			}
